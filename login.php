0a9f9df488d6f84e11127f0c4a7bb84600af38bf
jayalaxmidonka-n220414 <n220414@rguktn.ac.in>
2026-02-10 03:28:32 -0600

    Update login.php

--- login.php
+++ login.php
@@ -2,43 +2,31 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 session_start();
-
 $conn = new mysqli("localhost", "root", "", "cyberdb");
-
 if ($conn->connect_error) {
-    die("DB error: " . $conn->connect_error);
+    die("Database Connection Failed: " . $conn->connect_error);
 }
 
-echo "Connected to DB<br>";
-
 if (isset($_POST['login'])) {
-    echo "Form submitted<br>";
-
-    $email = $_POST['email'];
+    $email = trim($_POST['email']);
     $password = $_POST['password'];
-
-    echo "Email: $email<br>";
-    echo "Password received<br>";
 
     $sql = "SELECT * FROM users WHERE email = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $email);
     $stmt->execute();
-
     $result = $stmt->get_result();
-
-    echo "Rows found: " . $result->num_rows . "<br>";
 
     if ($result->num_rows === 1) {
         $user = $result->fetch_assoc();
-        if ($password !== $user['password']) {
-        echo "❌ Passwords do not match";
-        exit();
-        }else {
-            echo "✅ LOGIN SUCCESS";
+        if (strcmp($password, $user['password']) !== 0) {
+            echo "❌ Error: Passwords do not match."; 
+            exit();
+        } else {
+            echo "✅ LOGIN SUCCESS. WELCOME " . strtoupper($user['username']);
         }
     } else {
-        echo "❌ EMAIL NOT FOUND";
+        print "❌ Error: Email not found.";
     }
 }
 ?>
